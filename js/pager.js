898c93468d51b6a0d337ec4570039708623c8bf9
ShunSato <tamujun1231>
2016-12-11 23:30:26 +0900

    Initial commit

---
+++ js/pager.js
@@ -0,0 +1,82 @@
+function　pager(current, all, adress){
+  var url     = adress.toLowerCase();
+  var baseurl = url + '/page/';
+  var prev    = current - 1;
+  var prev2   = current - 2;
+  var next    = current + 1;
+  var next2   = current + 2;
+
+  if(all == 1){
+    $('#js-pager').append('<li> |< </li>');
+    $('#js-pager').append('<li> < </li>');
+    $('#js-pager').append('<li class="active"> 1 </li>');
+    $('#js-pager').append('<li>></a></li> ');
+    $('#js-pager').append('<li> >| </a></li>');
+  }else if(current == 1 && all == 2){
+    $('#js-pager').append('<li> |< </li>');
+    $('#js-pager').append('<li> < </li>');
+    $('#js-pager').append('<li class="active"> 1 </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + '2"> 2 </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '"> > </a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + all  + '"> >| </a></li>');
+  }else if(current == 1 && all > 2){
+    $('#js-pager').append('<li> |< </li>');
+    $('#js-pager').append('<li> < </li>');
+    $('#js-pager').append('<li class="active"> 1 </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + '2"> 2 </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + '3"> 3 </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '"> > </a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + all  + '"> >| </a></li>');
+  }else if(current == 2 && all-current == 1){
+    $('#js-pager').append('<li><a href="' + baseurl + '1"> |< </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev + '"> < </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '">' + prev + '</a></li>');
+    $('#js-pager').append('<li class="active">' + current + '</li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '">' + next + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '"> > </a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + all  + '"> >| </a></li>');
+  }else if(current == 2 && all-current == 0){
+    $('#js-pager').append('<li><a href="' + baseurl + '1"> |< </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev + '"> < </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '">' + prev + '</a></li>');
+    $('#js-pager').append('<li class="active">' + current + '</li>');
+    $('#js-pager').append('<li> > </li> ');
+    $('#js-pager').append('<li> >| </li>');
+  }else if (current == 2){
+    $('#js-pager').append('<li><a href="' + baseurl + '1"> |< </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '"> < </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '">' + prev + '</a></li>');
+    $('#js-pager').append('<li class="active">' + current + '</li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '">' + next + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next2 + '">' + next2 + '</a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '"> > </a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + all + '"> >| </a></li>');
+  }else if(all-current == 1){
+    $('#js-pager').append('<li><a href="' + baseurl + '1"> |< </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev + '"> < </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev2 + '">' + prev2 + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '">' + prev + '</a></li>');
+    $('#js-pager').append('<li class="active">' + current + '</li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '">' + next + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '"> > </a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + all  + '"> >| </a></li>');
+  }else if(all-current == 0){
+    $('#js-pager').append('<li><a href="' + baseurl + '1"> |< </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '"> < </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev2 + '">' + prev2 + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '">' + prev + '</a></li>');
+    $('#js-pager').append('<li class="active">' + current + '</li>');
+    $('#js-pager').append('<li> > </li> ');
+    $('#js-pager').append('<li> >| </li>');
+  }else{
+    $('#js-pager').append('<li><a href="' + baseurl + '1"> |< </li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '"> < </a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev2 + '">' + prev2 + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + prev  + '">' + prev + '</a></li>');
+    $('#js-pager').append('<li class="active">' + current + '</li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '">' + next + '</a></li>');
+    $('#js-pager').append('<li><a href="' + baseurl + next2 + '">' + next2 + '</a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + next + '"> > </a></li> ');
+    $('#js-pager').append('<li><a href="' + baseurl + all + '"> >| </a></li>');
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,181 @@
+
+//ページ内スクロール
+$(document).ready(function() {
+    $('a[href^="#"]').click(function(event) {
+
+        var id = $(this).attr("href");
+        var target = $(id).offset().top;
+        $('html, body').animate({scrollTop:target}, 500);
+        event.preventDefault();
+        return false;
+    });
+});
+
+/*Header menu*/
+$(document).ready(function(){
+	$(".Head__Toggle").click(function(){
+    	$(".Head__GlobalNavi").slideToggle("");
+	}).next().hide();
+});
+
+/*Contact*/
+$(document).ready(function () {
+  hsize = $(".Contact").outerHeight();
+  $(".Contact__Space").css("height", hsize + "px");
+});
+$(window).resize(function () {
+  hsize = $(".Contact").outerHeight();
+  $(".Contact__Space").css("height", hsize + "px");
+});
+
+
+/*Sideber accorfion*/
+$(document).ready(function(){
+	$(".Side__SlideToggle").click(function(){
+    	$(this).parent().toggleClass("Side__Title--Selected").next().slideToggle();
+	}).next().hide();
+});
+
+$(window).on('load resize',function(){
+	var windowWidth = $(window).width();
+	if(windowWidth>888){
+		$(".Side__SideWrap").show();
+	}
+
+	if(windowWidth<888){
+		$("#SideToggle .Side__SideWrap").hide();
+	}
+});
+
+
+window.onload=function(){
+	// ===================================
+	// HTMLChart
+	// ===================================
+	var data = [
+		{
+			value: 80,
+			color:"#000"
+		},
+		{
+			value : 20,
+	        // Draw with background color
+			color : "#eee"
+		}
+	];
+	var ctx = document.getElementById("HTMLChart").getContext("2d");
+	new Chart(ctx).Doughnut(data,{
+		segmentShowStroke : false,
+		segmentStrokeColor : "#fff",
+		segmentStrokeWidth : 1,
+		percentageInnerCutout : 92, // **** Border width
+		animation : true,
+		animationSteps : 100,
+		animationEasing : "easeOutBounce",
+		animateRotate : true,
+		animateScale : false,
+		onAnimationComplete : null
+	});
+
+	// ===================================
+	// CSSChart
+	// ===================================
+	var data = [
+		{
+			value: 75,
+			color:"#000"
+		},
+		{
+			value : 25,
+	        // Draw with background color
+			color : "#eee"
+		}
+	];
+	var ctx = document.getElementById("CSSChart").getContext("2d");
+	new Chart(ctx).Doughnut(data,{
+		segmentShowStroke : false,
+		segmentStrokeColor : "#fff",
+		segmentStrokeWidth : 1,
+		percentageInnerCutout : 92, // **** Border width
+		animation : true,
+		animationSteps : 100,
+		animationEasing : "easeOutBounce",
+		animateRotate : true,
+		animateScale : false,
+		onAnimationComplete : null
+	});
+
+	// ===================================
+	// JavascriptChart
+	// ===================================
+	var data = [
+		{
+			value: 60,
+			color:"#000"
+		},
+		{
+			value : 40,
+	        // Draw with background color
+			color : "#eee"
+		}
+	];
+	var ctx = document.getElementById("JavascriptChart").getContext("2d");
+	new Chart(ctx).Doughnut(data,{
+		segmentShowStroke : false,
+		segmentStrokeColor : "#fff",
+		segmentStrokeWidth : 1,
+		percentageInnerCutout : 92, // **** Border width
+		animation : true,
+		animationSteps : 100,
+		animationEasing : "easeOutBounce",
+		animateRotate : true,
+		animateScale : false,
+		onAnimationComplete : null
+	});
+}
+//LoadingBar
+$(function(){
+    var loadCount = 0,
+        imgLength = $("img").size();
+    $("img").each(function() {
+        var src = $(this).attr("src");
+        $("<img>")
+            .attr("src", src)
+            .load(function() {
+                loadCount++;
+            });
+    });
+
+    var timer = setInterval(function() {
+      console.log(loadCount);
+        $("#js-loadingBar").css({
+            "width": (loadCount / imgLength) * 100 + "%"
+        });
+    }, 5);
+});
+
+//ToTop
+$(function() {
+    var topBtn = $('#js-toTop');
+    //スクロールが100に達したら
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.addClass('Head__ToTop--Hover');
+        } else {
+            topBtn.removeClass('Head__ToTop--Hover');
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+//logo
+ $(function() {
+    $(".Top__Logo").fitText(0.5);
+    $(".Top__LogoSub__Cat").fitText(2.3);
+ });
